7640d5336fd4f29b2bfaa220650eb5103ec8019e
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:57:30 -0700

    Log ext dir

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -2,6 +2,7 @@
 
 install_ioncube_ext() {
   php_ext_dir=$(php-config --extension-dir)
+  echo $php_ext_dir
   curl -o ioncube.tar.gz "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
   tar -xzf ioncube.tar.gz
   cp -p ioncube/ioncube_loader_lin_5.6.so $php_ext_dir
