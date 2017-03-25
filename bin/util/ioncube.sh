32ad4d529889021cec1003ade412c9679a027041
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:30:10 -0700

    Use ioncube loader 5.6

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -3,6 +3,6 @@
 install_ioncube_ext() {
   curl -o ioncube.tar.gz "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
   tar -xzf ioncube.tar.gz
-  cp -p ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR
-  ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
+  cp -p ioncube_loader_lin_5.6.so $PHP_EXT_DIR
+  ln -s $PHP_EXT_DIR/ioncube_loader_lin_5.6.so $PHP_EXT_DIR/ioncube.so
 }
