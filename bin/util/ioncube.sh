cc5e9ca26de9a69daaec34950f8bb403c1ffb7c4
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:43:15 -0700

    Fix ioncube path

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -4,6 +4,7 @@ install_ioncube_ext() {
   php_ext_dir="$build_dir/.heroku/php/usr/lib/php/modules"
   curl -o ioncube.tar.gz "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
   tar -xzf ioncube.tar.gz
-  cp -p ioncube_loader_lin_5.6.so $php_ext_dir
+  cp -p ioncube/ioncube_loader_lin_5.6.so $php_ext_dir
+  rm -rf ioncube ioncube.tar.gz
   ln -s $php_ext_dir/ioncube_loader_lin_5.6.so $php_ext_dir/ioncube.so
 }
