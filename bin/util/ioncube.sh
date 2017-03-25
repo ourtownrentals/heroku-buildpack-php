00b2ad929187b0e3a4203eab2dbbd36bc092abe1
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:26:26 -0700

    Update ioncube script

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 install_ioncube_ext() {
-  install_ext "ioncube" "automatic" \
-    "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
-    ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
+  curl -o ioncube.tar.gz "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
+  tar -xzf ioncube.tar.gz
+  cp -p ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR
+  ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
 }
