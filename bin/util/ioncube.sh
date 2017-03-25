6d27860f80557c890576cab62f478d3eaaa8e3c2
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:41:23 -0700

    Set php_ext_directory

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 install_ioncube_ext() {
+  php_ext_dir="$build_dir/.heroku/php/usr/lib/php/modules"
   curl -o ioncube.tar.gz "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
   tar -xzf ioncube.tar.gz
-  cp -p ioncube_loader_lin_5.6.so $PHP_EXT_DIR
-  ln -s $PHP_EXT_DIR/ioncube_loader_lin_5.6.so $PHP_EXT_DIR/ioncube.so
+  cp -p ioncube_loader_lin_5.6.so $php_ext_dir
+  ln -s $php_ext_dir/ioncube_loader_lin_5.6.so $php_ext_dir/ioncube.so
 }
