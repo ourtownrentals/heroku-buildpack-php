dda48c1bb42dcb967a1274520b27d7778d185054
Evan Sosenko <razorx@evansosenko.com>
2017-03-25 15:00:12 -0700

    Add ionCube

---
+++ bin/util/ioncube.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+install_ioncube_ext() {
+  if [[ ( ${#exts[@]} -eq 0 || ! ${exts[*]} =~ "ioncube" ) ]]; then
+    install_ext "ioncube" "automatic" "https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz"
+    exts+=("ioncube")
+
+    ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
+  fi
+}
